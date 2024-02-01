42e633ebb4b7612e5c32329fefbb559470fb3a6f
kengkeng852 <88934262+kengkeng852@users.noreply.github.com>
2024-02-01 21:52:55 +0700

    Delete Digit.js

--- Digit.js
+++
@@ -1,8 +0,0 @@
-import {actions} from './App';
-
-export default function Digit({dispatch,digit}) {
-    return (
-    <button onClick={() => dispatch({type: actions.add_digit, payload: {digit} })}>
-        {digit}
-    </button>);
-}
